aca33a1e9120fda0758fc20b31c7d5be843a8cb0
Dipesh Acharya <xtranophilist@gmail.com>
2013-03-01 14:16:47 +0545

    created basic movie info view page

--- movies/urls.py
+++ movies/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import patterns, url
 from movies import views
 
 urlpatterns = patterns('',
-     url(r'^$', views.index, name='index'),
-     url(r'^list$', views.list, name='list')
-)
+    url(r'^$', views.index, name='index'),
+    url(r'', views.movie, name='movie')
+    )
--- movies/views.py
+++ movies/views.py
@@ -1,18 +1,22 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
-from imdb import IMDb
+from models import Movie
 
 
 @login_required
 def index(request):
-    ia = IMDb('sql', uri='mysql://root:passweird@localhost/reelphil')
-    movie = ia.get_movie('1318514')
-    print movie['title']
+    return render(request, 'movies/all.html', {"movie": movie})
+
+
+def movie(request):
+    slug = request.path.replace('/movies/', '')
+    movie = Movie.objects.get(slug=slug)
+    print movie.id
     return render(request, 'movies/movie.html', {"movie": movie})
 
 
-def list(request):
-    ia = IMDb('sql', uri='mysql://root:passweird@localhost/reelphil')
-    # ia = IMDb()
-    list = ia.get_top250_movies()
-    return render(request, 'movies/list.html', {"list": list})
+# def list(request):
+#     ia = IMDb('sql', uri='mysql://root:passweird@localhost/reelphil')
+#     # ia = IMDb()
+#     list = ia.get_top250_movies()
+#     return render(request, 'movies/list.html', {"list": list})
