2f9acf3dbe0499b9e74f7f026feb64b61b3b9cd8
Dipesh Acharya <xtranophilist@gmail.com>
2013-03-27 14:32:01 +0545

    added profile_image field for UserProfile

--- reelphil/settings.py
+++ reelphil/settings.py
@@ -174,7 +174,7 @@ SOCIAL_AUTH_FORCE_RANDOM_USERNAME = False
 SOCIAL_AUTH_DEFAULT_USERNAME      = 'socialauth_user'
 SOCIAL_AUTH_COMPLETE_URL_NAME     = 'complete'
 LOGIN_ERROR_URL                   = '/login/error/'
-#SOCIAL_AUTH_USER_MODEL            = 'app.CustomUser'
+# SOCIAL_AUTH_USER_MODEL            = 'users.CustomUser'
 SOCIAL_AUTH_ERROR_KEY             = 'socialauth_error'
 
 
--- users/models.py
+++ users/models.py
@@ -1,19 +1,57 @@
 from django.db import models
 from django.contrib.auth.models import User
 from movie.models import Checkin
+import os
+
+
+def get_image_path(instance, filename):
+    return os.path.join('uploads/profile_images', str(instance.id), filename)
 
 
 class Profile(models.Model):
     user = models.ForeignKey(User, unique=True)
     full_name = models.CharField(max_length=200, blank=True)
     following = models.ManyToManyField('self', related_name='followers', symmetrical=False)
+    profile_image = models.ImageField(upload_to=get_image_path, blank=True, null=True)
 
     def get_absolute_url(self):
         return ('profiles_profile_detail', (), {'username': self.user.username})
     get_absolute_url = models.permalink(get_absolute_url)
 
-    def get_checkins(self):
-        checkins = Checkin.objects.filter(user=self.user)
-        return checkins
 
-    checkins = property(get_checkins)
+from social_auth.backends.facebook import FacebookBackend
+from social_auth.backends import google
+from social_auth.signals import socialauth_registered
+
+
+def new_users_handler(sender, user, response, details, **kwargs):
+    user.is_new = True
+    print 'aha'
+    if user.is_new:
+        if "id" in response:
+
+            from urllib2 import urlopen, HTTPError
+            from django.template.defaultfilters import slugify
+            from django.core.files.base import ContentFile
+
+            try:
+                url = None
+                if sender == FacebookBackend:
+                    url = "http://graph.facebook.com/%s/picture?type=large" % response["id"]
+                elif sender == google.GoogleOAuth2Backend and "picture" in response:
+                    url = response["picture"]
+
+                if url:
+                    avatar = urlopen(url)
+                    profile = Profile(user=user)
+
+                    profile.profile_photo.save(slugify(user.username + " social") + '.jpg', ContentFile(avatar.read()))
+
+                    profile.save()
+
+            except HTTPError:
+                pass
+
+    return False
+
+socialauth_registered.connect(new_users_handler, sender=None)
