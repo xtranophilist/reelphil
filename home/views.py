269423f85d28d167a45de1260dd20b52e0fb2d54
Dipesh Acharya <xtranophilist@gmail.com>
2013-03-27 13:42:34 +0545

    fixed home view for anonymous users

--- home/views.py
+++ home/views.py
@@ -1,13 +1,14 @@
 from django.shortcuts import render
 from movie.models import Activity
 from movie.serializers import ActivitySerializer
-from users.models import Profile
 from django.db.models import Q
 
 
 def index(request):
-    profile = Profile.objects.get(user=request.user)
-    following = [prof.user for prof in profile.following.all()]
-    obj = Activity.objects.filter(Q(user__in=following) | Q(user=request.user)).order_by('-id')[:10]
+    if request.user.is_authenticated():
+        followings = [following.user for following in request.user.get_profile().following.all()]
+        obj = Activity.objects.filter(Q(user__in=followings) | Q(user=request.user)).order_by('-id')[:10]
+    else:
+        obj = Activity.objects.all().order_by('-id')[:10]
     timeline = ActivitySerializer(obj).data
     return render(request, 'home.html', {'timeline': timeline})
